f503a43924295f75437627280158f7fb1b57c0e4
James Sturtevant <jsturtevant@gmail.com>
2020-12-05 06:38:16 +0000

    day4 part2

--- day4/main.go
+++ day4/main.go
@@ -4,13 +4,10 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
 )
-
-type slope struct {
-	right int
-	down  int
-}
 
 func main() {
 
@@ -31,33 +28,142 @@ func countValidPassprts(input string) int {
 	passports := strings.Split(input, "\n\n")
 
 	validCount := 0
-	for _, p := range passports {
+	for _, rawPassport := range passports {
+		p := newPassport(rawPassport)
 
-		f := strings.Fields(p)
-
-		if len(f) == 8 {
-			fmt.Println(p)
-			fmt.Println("Valid! All eight fields.")
+		if p.validate() {
 			validCount = validCount + 1
-			continue
-		}
-
-		if len(f) == 7 {
-			found := false
-			for _, v := range f {
-				if strings.Contains(v, "cid:") {
-					found = true
-				}
-			}
-
-			if !found {
-				fmt.Println(p)
-				fmt.Println("Valid! Only missing cid.")
-				validCount = validCount + 1
-				continue
-			}
 		}
 	}
 
 	return validCount
+}
+
+type passport struct {
+	fields map[string]string
+}
+
+func validateDate(v string, min, max int) bool {
+	if len(v) != 4 {
+		return false
+	}
+	date, err := strconv.Atoi(v)
+	if err != nil {
+		return false
+	}
+	if date < min || date > max {
+		return false
+	}
+
+	return true
+}
+
+func validateHieght(v string) bool {
+	re := regexp.MustCompile(`([0-9]*)(in|cm)`)
+	matches := re.FindStringSubmatch(v)
+	if len(matches) != 3 {
+		return false
+	}
+
+	mt := matches[2]
+	mm, _ := strconv.Atoi(matches[1])
+	switch mt {
+	case "in":
+		if mm < 59 || mm > 76 {
+			return false
+		}
+	case "cm":
+		if mm < 150 || mm > 193 {
+			return false
+		}
+	default:
+		//unkown
+		return false
+	}
+
+	return true
+}
+
+func (p *passport) validate() bool {
+	if len(p.fields) < 7 {
+		return false
+	}
+
+	found := false
+	for key, value := range p.fields {
+		switch key {
+		case "byr":
+			isValid := validateDate(value, 1920, 2002)
+			if !isValid {
+				return false
+			}
+		case "iyr":
+			isValid := validateDate(value, 2010, 2020)
+			if !isValid {
+				return false
+			}
+		case "eyr":
+			isValid := validateDate(value, 2020, 2030)
+			if !isValid {
+				return false
+			}
+		case "hgt":
+			isValid := validateHieght(value)
+			if !isValid {
+				return false
+			}
+		case "hcl":
+			matched, _ := regexp.MatchString(`^#[0-9|a-f]{6,6}$`, value)
+			if !matched {
+				return false
+			}
+		case "ecl":
+			matched, _ := regexp.MatchString(`^(amb|blu|brn|gry|grn|hzl|oth)$`, value)
+			if !matched {
+				return false
+			}
+		case "pid":
+			matched, _ := regexp.MatchString(`^[0-9]{9,9}$`, value)
+			if !matched {
+				return false
+			}
+		case "cid":
+			found = true
+			continue
+		default:
+			// unkown key
+			return false
+		}
+	}
+
+	if len(p.fields) == 8 {
+		fmt.Println(p)
+		fmt.Println("Valid! All eight fields are valid.")
+		return true
+	}
+
+	if len(p.fields) == 7 {
+		if !found {
+			fmt.Println(p)
+			fmt.Println("Valid! Fields valid, only missing cid.")
+			return true
+		}
+	}
+
+	// unknown
+	return false
+}
+
+func newPassport(rawPassport string) passport {
+	fields := strings.Fields(rawPassport)
+
+	m := make(map[string]string)
+	for _, f := range fields {
+		record := strings.Split(f, ":")
+		m[record[0]] = record[1]
+	}
+
+	return passport{
+		fields: m,
+	}
 }
